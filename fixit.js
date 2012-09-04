7409ea5d0bd755189c26bf562723ff22fdc895cd
Ger Hobbelt <ger@hobbelt.com>
2012-09-04 17:19:56 +0200

    basic implementation of the fixit code for styling the bl.ocks.org page so it looks good with longer titles and such

--- fixit.js
+++ fixit.js
@@ -1,19 +1,46 @@
-<style>
-h1 { /* fix bl.ocks.org ugly <h1> */
-  font-size: 28px;
-  margin-top: 2em;
-  margin-bottom: .5em;
+
+function fix_blocksorg() {
+  var frame = null;
+  var frame_doc = null;
+  var tweaker = function() {
+    return tweaker;
+  };
+  
+  tweaker.adjust_gist_iframe(width, height) {
+    if (width > 0 && d3) {
+      d3.select(frame)
+        .style("width", width);
+    }
+    if (height > 0 && d3) {
+      d3.select(frame)
+        .style("height", height);
+    }
+    return tweaker;
+  }
+  
+  try {
+    frame = window.frameElement;
+    if (!frame)
+      return;
+    frame_doc = frame.ownerDocument;
+    if (!frame_doc)
+      return;
+
+    /* fix bl.ocks.org ugly <h1> */
+    var style = frame_doc.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = 'h1.gist-title { font-size: 48px; letter-spacing: -2px; margin: .3em 4em .1em 0; line-height: 1em; }';
+    frame_doc.getElementsByTagName('head')[0].appendChild(style);
+    
+    var style = frame_doc.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.gist-readme h1 { font-size: 28px; letter-spacing: 0px; margin: 2em 5em .5em 0; line-height: 1.1em; font-weight: bold; }';
+    frame_doc.getElementsByTagName('head')[0].appendChild(style);
+    
+  } catch (e) {
+    // forget about it...
+  }
+  
+  return tweaker;
 }
-</style>
-
-TODO …… <a href="http://bl.ocks.org/2997144" title="view this gist via bl.ocks.org">?</a> <a href="http://bl.ocks.org/GerHobbelt" title="view a list of all my gists at bl.ocks.org">?</a>
-
-
-
-
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = '.cssClass { color: #F00; }';
-document.getElementsByTagName('head')[0].appendChild(style);
-
-document.getElementById('someElementId').className = 'cssClass';
+  
